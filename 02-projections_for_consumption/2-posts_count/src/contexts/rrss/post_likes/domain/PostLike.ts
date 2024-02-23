1a9d4f2946e9fa0be68e9a646097b6a6621f6e9f
Rafa Gómez <rgomezcasas@gmail.com>
2024-02-23 17:23:46 +0100

    feat(3-2): test post liker

--- 02-projections_for_consumption/2-posts_count/src/contexts/rrss/post_likes/domain/PostLike.ts
+++ 02-projections_for_consumption/2-posts_count/src/contexts/rrss/post_likes/domain/PostLike.ts
@@ -1,4 +1,7 @@
+import { Primitives } from "@codelytv/primitives-type";
+
 import { AggregateRoot } from "../../../shared/domain/AggregateRoot";
+import { Clock } from "../../../shared/domain/Clock";
 import { PostId } from "../../posts/domain/PostId";
 import { UserId } from "../../users/domain/UserId";
 import { PostLikedDomainEvent } from "./PostLikedDomainEvent";
@@ -7,23 +10,41 @@ import { PostLikeId } from "./PostLikeId";
 export class PostLike extends AggregateRoot {
 	private constructor(
 		public readonly id: PostLikeId,
-		private readonly postId: PostLikeId,
-		private readonly userId: UserId,
-		private readonly likedAt: Date,
+		public readonly postId: PostLikeId,
+		public readonly userId: UserId,
+		public readonly likedAt: Date,
 	) {
 		super();
 	}
 
-	static like(id: string, postId: string, userId: string): PostLike {
+	static like(id: string, postId: string, userId: string, clock: Clock): PostLike {
 		const post = new PostLike(
-			new UserId(userId),
 			new PostLikeId(id),
 			new PostId(postId),
-			new Date(),
+			new UserId(userId),
+			clock.now(),
 		);
 
 		post.record(new PostLikedDomainEvent(id, postId, userId));
 
 		return post;
+	}
+
+	static fromPrimitives(primitives: Primitives<PostLike>): PostLike {
+		return new PostLike(
+			new PostLikeId(primitives.id),
+			new PostId(primitives.postId),
+			new UserId(primitives.userId),
+			primitives.likedAt as Date,
+		);
+	}
+
+	toPrimitives(): Primitives<PostLike> {
+		return {
+			id: this.id.value,
+			userId: this.userId.value,
+			postId: this.postId.value,
+			likedAt: this.likedAt,
+		};
 	}
 }
